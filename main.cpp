b7a1854405556d6dacfc35811b47dc74dea2e5bb
Chen-Chi Hu <chenchih.hu@gmail.com>
2015-12-04 23:57:00 +0800

    draw diamond!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
-
 using namespace std;
-int test(){
-    cout << "test" << endl;
-    return 100;
-}
 int main()
 {
-    //for(int i=0;i<20;i++)cout << i*i << endl;
-    cout << test() << endl;
-    return 0;
+int i;
+
+       for( i=0;i<3;i++)
+       {
+               for(int j=3;j>i;j--)
+               cout<<" ";
+                       for(int k=0;k<i;k++)
+                       cout<<" *";
+                       cout<<endl;
+       }
+       for(i=3;i>0;i--)
+       {
+               for(int j=3;j>i;j--)
+               cout<<" ";
+                       for(int k=0;k<i;k++)
+                       cout<<" *";
+                       cout<<endl;
+
+       }
+       for(int i=0;i<3;i++)
+       {
+               for(int j=3;j>i;j--)
+               cout<<" ";
+                       for(int k=0;k<i;k++)
+                       cout<<" *";
+                       cout<<endl;
+
+
+        return 0;
+       }
 }
