6debed839791cd3e44f9cbff4f1183059fb15ad9
Chen-Chi Hu <chenchih.hu@gmail.com>
2015-12-04 23:54:51 +0800

    function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
+int test(){
+    cout << "test" << endl;
+    return 100;
+}
 int main()
 {
-    for(int i=0;i<20;i++)cout << i*i << endl;
+    //for(int i=0;i<20;i++)cout << i*i << endl;
+    cout << test() << endl;
     return 0;
 }
